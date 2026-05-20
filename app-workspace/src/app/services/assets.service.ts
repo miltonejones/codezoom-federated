ae2d156b45723e6217499a205dcc3d104823977d
Milton Jones <miltonejones@gmail.com>
2026-05-20 11:38:53 -0500

    adding env vars

--- app-workspace/src/app/services/assets.service.ts
+++ app-workspace/src/app/services/assets.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AssetsService {
-  private get baseUrl(): string {
-    // Use localhost for development, Azure URL for production
-    if (window.location.hostname === 'localhost') {
-      return 'http://localhost:4203';
-    }
-    return 'https://yellow-water-00b488b0f-dev.eastus2.7.azurestaticapps.net'; //window.location.origin;
-  }
-
   getImageUrl(imagePath: string): string {
-    return `${this.baseUrl}/${imagePath}`;
+    return `${environment.assetsUrl}/${imagePath}`;
   }
 }
